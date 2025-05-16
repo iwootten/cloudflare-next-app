25a65220a87e48d7305cb86d436f1b18dd6674f0
Ian Wootten <3598161+iwootten@users.noreply.github.com>
2025-05-16 13:53:37 +0100

    Test out a shadcn landing page

---
+++ app/pricing/page.tsx
@@ -0,0 +1,11 @@
+import { HeroHeader } from "@/components/header";
+import Pricing from "@/components/pricing";
+
+export default function Home() {
+  return (
+    <>
+      <HeroHeader />
+      <Pricing />
+    </>
+  );
+}
